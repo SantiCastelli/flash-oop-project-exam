de7fe63b9a855ef8e4090a934a3597693c6263d1
omiras <oscar.miras@gmail.com>
2022-04-16 10:56:13 +0200

    req 4

--- js/app.js
+++ js/app.js
@@ -1,19 +1,58 @@
-let nextId = 3;
+let nextId = 0;
+
+// Declarar de manera explícita los posibles estados del formulario nos evita tener que ir jugando con valores true/false e incluso nos ahorra variables
+const FORM_MODE = {
+    hidden: 0,
+    add: 1,
+    edit: 2
+}
 
 
 Vue.createApp({
     data() {
         return {
-            notes: [{
-                id: 1,
-                question: "¿Cómo hago para iterar un array con Vue?",
-                answer: "Usa la directiva v-for"
-            }, {
-                id: 2,
-                question: "¿Cómo hago para relacionar una variable de estado con el campo <em>value</em>' de input?",
-                answer: "La directiva v-model nos ayudará en este caso"
-
-            }]
+            notes: [],
+            formMode: FORM_MODE.hidden,
+            // Puesto que una nota se compone justamente de una pregunta y una respuesta, un objeto es una estructura adecuada para mantener sus dos propiedades juntas
+            note: {
+                id: nextId++,
+                question: '',
+                answer: ''
+            }
         }
-    }
+    },
+    methods: {
+        onAddQuestion() {
+            // Queda muy explícita que estamos en modo 'añadir nueva nota'
+            this.formMode = FORM_MODE.add;
+        },
+        // Ocultar el formulario es una funcionalidad que se repite en muchos requisitos, bueno tenerlo en un método
+        onCloseForm() {
+            this.formMode = FORM_MODE.hidden;
+        },
+        onAddNote() {
+            this.notes.push(this.note); // añadimos la nota que estamos editando, no olvidemos añadir el identificador
+            this.note = {
+                id: nextId++,
+                question: '',
+                answer: ''
+            }; // limpiamos la nota que estamos editando y preparamos el siguiente id
+            this.formMode = FORM_MODE.hidden; // ocultamos el formulario
+        },
+        // Pasar el objeto entero es gratis, hacedlo siempre. Luego ya nos quedamos con los campos que nos interesa
+        onDelete(note) {
+            this.notes = this.notes.filter(n => note.id != n.id)
+        }
+    },
+    computed:
+    {
+        // En qué casos tenemos que mostrar el formulario?
+        mustShowForm() {
+            return this.formMode == FORM_MODE.add;
+        }
+    },
+
+
+
+
 }).mount('#app')
